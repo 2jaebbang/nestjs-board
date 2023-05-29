7d60e0b2be558e01bf4bdf8c4e8323df2433de5d
2jaebbang <2jaebbang@naver.com>
2023-05-29 17:01:02 +0900

    Feat: 해당 유저의 게시글만 조회하도록 로직 변경

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -9,8 +9,12 @@ import { User } from '@/auth/user.entity';
 export class BoardsService {
   constructor(private boardRepository: BoardRepository) {}
 
-  async getAllBoards(): Promise<Board[]> {
-    return this.boardRepository.find();
+  async getAllBoards(user: User): Promise<Board[]> {
+    const query = this.boardRepository.createQueryBuilder('board');
+    query.where('board.userId= :userId', { userId: user.id });
+
+    const boards = await query.getMany();
+    return boards;
   }
 
   async getBoardById(id: number): Promise<Board> {
