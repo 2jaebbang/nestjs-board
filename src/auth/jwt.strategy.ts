c19091038a310a6c24ec789e68bccfcfb961ad9d
2jaebbang <2jaebbang@naver.com>
2023-05-29 22:18:06 +0900

    Feat: config 적용

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -3,14 +3,14 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserRepository } from '@auth/user.repository';
 import { PassportStrategy } from '@nestjs/passport';
 import { User } from '@auth/user.entity';
+import * as config from 'config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private userRepository: UserRepository) {
     super({
       //토큰 유효한지 체크히기 위해 선언
-      secretOrKey: 'Secret1234',
-
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
       //토큰이 클라이언트의 어느부분에서 오는지 체크
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
