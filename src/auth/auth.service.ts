f6315b48da164750916a8d7caab2dad900219ce8
2jaebbang <2jaebbang@naver.com>
2023-05-29 12:35:09 +0900

    Feat: 회원가입 기능 api 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { UserRepository } from './user.repository';
+import { UserRepository } from '@auth/user.repository';
+import { AuthCredentialsDto } from '@auth/dto/auth-credential.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private boardRepository: UserRepository) {}
+  constructor(private userRepository: UserRepository) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.createUser(authCredentialsDto);
+  }
 }
