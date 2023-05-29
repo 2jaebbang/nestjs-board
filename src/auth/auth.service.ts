f5a3d0ace71529cc52f78610907cd63c059bb6b5
2jaebbang <2jaebbang@naver.com>
2023-05-29 15:52:11 +0900

    Feat: JWT토큰 발급 로직 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,20 +2,31 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserRepository } from '@auth/user.repository';
 import { AuthCredentialsDto } from '@auth/dto/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.userRepository.findOneBy({ username });
+
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      //유저 토큰 생성 (Secret + Payload)
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return { accessToken };
     } else {
       throw new UnauthorizedException();
     }
