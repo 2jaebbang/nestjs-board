e1f2546f3c18de37490c85965fa742fb6294f368
2jaebbang <2jaebbang@naver.com>
2023-05-29 17:18:50 +0900

    Feat: 자신이 생성한 게시글만 삭제할 수 있도록 로직 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -36,8 +36,13 @@ export class BoardsService {
     return board;
   }
 
-  async deleteBoard(id: number): Promise<void> {
-    const result = await this.boardRepository.delete(id);
+  async deleteBoard(id: number, user: User): Promise<void> {
+    const result = await this.boardRepository
+      .createQueryBuilder('board')
+      .delete()
+      .where('id = :id', { id })
+      .andWhere('userId = :userId', { userId: user.id })
+      .execute();
 
     if (result.affected === 0) {
       throw new NotFoundException(`Can't find Board with id ${id}`);
